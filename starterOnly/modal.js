cc0e80c8321d77642e032af5bec2b88114d15e19
Arnauud <arnaud.bourgin@gmail.com>
2024-05-05 23:33:54 +0200

    Closing Modal popup using javascript

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modalClose = document.querySelector(".close") // issue number 1 = calling up the span.
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -20,4 +21,11 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+
+// issue number 1 to close the modal 
+
+modalClose.addEventListener("click", closeModal) // upon a click to parameter closeModal function
+function closeModal (){ 
+  modalbg.style.display = "none"; // using queryselector .bgroud to 
+}
 
