cd8c3e8a20737a1fdcb9f14484d7b7692c6fee06
Arnauud <arnaud.bourgin@gmail.com>
2024-05-10 18:43:43 +0200

    index + css page update for error managment JS updated for error management : missing general condition and location selection + CSS for last page

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -31,70 +31,131 @@ function closeLaunchModal (){
 
 // ISSUE #2 IMPLEMENTER ENTREES DU FORMULAIRE + ISSUE #3 Ajouter validation ou messages d'erreur
 
-// let validationChecks = {
-//   firstNameCheck: false,
-//   lastNameCheck : false,
-//   emailValidation : false,
-//   contestNumberValidation : false,
-//   locationisChecked : false,
-//   conditionUsageCheck : false,
-//   dateOfBirthCheck : false,
-// }
-
-let firstNameCheck = false;
-let lastNameCheck = false;
-let emailValidationCheck = false;
-let contestNumberValidationCheck = false;
-let locationisChecked = false;
-let conditionUsageCheck = false;
-let dateOfBirthCheck = false;
+let errorMessage = {
+  firstName : "Veuillez entrer 2 caractères ou plus pour le champ du Prénom",
+  lastName : "Veuillez entrer 2 caractères ou plus pour le champ du Nom",
+  email : "Rentrez un e-mail valide",
+  contestNumber : "Merci de bien vouloir indiquer un chiffre",
+  location : "Merci de bien vouloir choisir un lieu",
+  ToU : "Merci d'accpeter les conditions générales d'utilistation",
+  dateOfBirth : "Merci de bien vouloir saisir votre date de naissance",
+};
 
 
 // (1) Le champ Prénom a un minimum de 2 caractères / n'est pas vide.
 function firstNameValidation (){
-  let firstName = document.getElementById("first").value;
+  let firstNameInput = document.getElementById("first");
+  let errorMessageElement = document.getElementById("first-error")
+  let firstName= firstNameInput.value
 
   if(firstName.length > 2 && firstName !== '' ){
-      console.log("Good first name")
-      firstNameCheck = true
+
+    console.log("Good first name")
+    firstNameCheck = true
+    errorMessageElement.style.display = "none";
+    firstNameInput.classList.remove("error")
+
     } else {
-      console.log("Veuillez entrer 2 caractères ou plus pour le champ du Prénom")
+      console.log(errorMessage.firstName)
+      firstNameCheck = false
+  
+       // Set error message text
+      errorMessageElement.style.display = "block";
+      firstNameInput.classList.add("error")
+      errorMessageElement.textContent = errorMessage.firstName;
+
     }
-}
-console.log(firstNameCheck)
+};
+
+
+
 // (2) Le champ du nom de famille a un minimum de 2 caractères / n'est pas vide.
 
 function lastNameValidation (){
-  let lastName = document.getElementById("last").value;
+  let lastNameInput = document.getElementById("last");
+  let errorMessageElement = document.getElementById("last-error");
+  let lastName = lastNameInput.value;
+
   if(lastName.length > 2 && lastName !== '' ){
     console.log("Good last name")
     lastNameCheck = true
+    errorMessageElement.style.display = "none";
+    lastNameInput.classList.remove("error")
   } else {
-    console.log("Veuillez entrer 2 caractères ou plus pour le champ du Nom")
+    console.log(errorMessage.lastName)
+    lastNameCheck = false
+
+    // Set error message text
+    errorMessageElement.style.display = "block";
+    lastNameInput.classList.add("error")
+    errorMessageElement.textContent = errorMessage.lastName;
   }
-}
+};
 
 // (3) L'adresse électronique est valide.
 
 function emailValidation (){
-  let email = document.getElementById("email").value;
+  let emailInput = document.getElementById("email");
   let emailRegExp = new RegExp ("[a-z0-9._-]+@[a-z0-9._-]+\\.[a-z0-9._-]+");
+  let errorMessageElement = document.getElementById("email-error");
+  let email = emailInput.value
+
   if (emailRegExp.test(email)){
     console.log("Good email name")
-    let emailValidationCheck = true
+    emailValidationCheck = true
+    errorMessageElement.style.dispaly = "none"
+    emailInput.classList.remove("error")
+
   } else {
-    console.log("Rentrez un e-mail valide")
+    console.log(errorMessage.email);
+    emailValidationCheck = false;
+    errorMessageElement.style.display = "block";
+   
+    emailInput.classList.add("error");
+    errorMessageElement.textContent = errorMessage.email;
   }
+};
+
+// BONUS Date de Naissance
+function dateOfBirth(){
+  let birthdateInput = document.getElementById("birthdate");
+  let birthdate = birthdateInput.value;
+
+  let dateOfBirthError = document.getElementById("dob-error");
+
+if(birthdate !== ''){
+  dateOfBirthCheck = true;
+  console.log("Ok date of birth");
+  birthdateInput.classList.remove("error");
+  dateOfBirthError.style.display ="none";
+}else{
+  console.log(errorMessage.dateOfBirth);
+  dateOfBirthCheck = false;
+  birthdateInput.classList.add("error");
+  dateOfBirthError.style.display ="block";
+  dateOfBirthError.textContent = errorMessage.dateOfBirth;
 }
+};
 
 // (4) Pour le nombre de concours, une valeur numérique est saisie.
 function contestNumberValidation(){
-  let contestNumberValidation = document.getElementById("quantity").value;
-  if (contestNumberValidation === ''){
-    console.log("Need to put a 0 at least")
-    let contestNumberValidationCheck = true
+  let contestNumberValidation = document.getElementById("quantity");
+  let contestNumber = contestNumberValidation.value;
+  let ErrorMessageValidation = document.getElementById("quantity-error");
+
+  if (contestNumber === '' ){
+    console.log(errorMessage.contestNumber);
+    contestNumberValidation.classList.add("error");
+    contestNumberValidationCheck = false;
+    ErrorMessageValidation.style.display = "block";
+    ErrorMessageValidation.textContent = errorMessage.contestNumber;
+
   } else {
-    console.log(`Amount of concerts attended to : ${contestNumberValidation}`)
+    contestNumberValidation.classList.remove("error");
+    ErrorMessageValidation.style.display = "none";
+    console.log(`Accepted Number amount of concerts`);
+    contestNumberValidationCheck = true;
+
   }
   
 
@@ -104,54 +165,47 @@ function contestNumberValidation(){
 
 function locationButton(){
   let locationButton = document.querySelectorAll('input[type="radio"]');
-for (let i = 0; i < locationButton.length; i++)
+for (let i = 0; i < locationButton.length; i++){
   if(locationButton[i].checked){
-    locationisChecked = true
-  }
-
-  if (locationisChecked === true){
-    console.log("location has been checked")
-    
+   locationisChecked = true
+   break;
   } else {
-    console.log("you NEED TO have location checked")
+    locationisChecked = false
   }
+}
 
-
+  if (locationisChecked){
+    console.log("location has been checked")
+  } else {
+    console.log(errorMessage.location)
+    locationisChecked = false
+  }
 };
+
+
 // (6) La case des conditions générales est cochée, l'autre case est facultative / peut être laissée décochée.
 function conditionAccepted(){
-  let conditionUsage = document.getElementById("checkbox1")
-  let conditionPromo = document.getElementById("checkbox2")
+  let conditionUsage = document.getElementById("checkbox1");
+  let conditionPromo = document.getElementById("checkbox2");
   if (!conditionUsage.checked) {
-    console.log("Have to Select Usage or else...!!!")
-      conditionUsageCheck = true
+    conditionUsage.style.display = "block";
+    conditionUsage.textContent = errorMessage.ToU;
+    conditionUsageCheck = false;
   } else if (conditionUsage.checked && conditionPromo.checked){
-    console.log("Double win | Usage & Promotion")
-      conditionUsageCheck = true
+    conditionUsage.style.display = "none";
+    console.log("Double win | Usage & Promotion");
+    conditionUsageCheck = true;
   } else if (conditionUsage.checked && !conditionPromo.checked){
-    console.log("Single Win | At least the took the Usage")
+    conditionUsage.style.display = "none";
+    console.log("Single Win | At least the took the Usage");
+    conditionUsageCheck = true;
   }
 };
 
-// BONUS Date de Naissance
-function dateOfBirth(){
-  let birthdate = document.getElementById("birthdate").value;
-if(birthdate !== ''){
-  let dateOfBirthCheck = true
-  console.log("Ok date of birth")
-}else{
-  console.log("Merci de bien vouloir saisir votre date de naissance")
-}
-}
+
 
 function thankYou(){
-if (firstNameCheck && 
-  lastNameCheck && 
-  emailValidationCheck && 
-  contestNumberValidationCheck &&  
-  locationisChecked &&
-  conditionUsageCheck &&
-  dateOfBirthCheck === true){
+if (firstNameCheck && lastNameCheck && emailValidationCheck && contestNumberValidationCheck && locationisChecked && conditionUsageCheck && dateOfBirthCheck === true){
   alert("Merci! Votre réservation a été reçue.")
 } else {
   alert("Stuff needs to get worked out")
@@ -170,8 +224,8 @@ function formValidation (){
       firstNameValidation(); 
       lastNameValidation();
       emailValidation();
-      dateOfBirth();
       contestNumberValidation();
+      dateOfBirth();
       locationButton();
       conditionAccepted();
       thankYou();
